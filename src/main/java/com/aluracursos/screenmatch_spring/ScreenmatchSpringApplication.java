42f1aeb3eeb07907599697f525130eee7f34df60
Sonia Suárez <ssuarezcode@outlook.com>
2024-07-03 13:19:21 -0500

    Adding Spring template.

---
+++ src/main/java/com/aluracursos/screenmatch_spring/ScreenmatchSpringApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/screenmatch_spring/ScreenmatchSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
